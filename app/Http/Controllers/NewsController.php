58ddfd6c6149799129aef9ae64b6f92a68a57da9
faisal <faisal@mvp-apps.ae>
2022-07-13 15:01:34 +0400

    fix:paginate

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,6 +9,6 @@ class NewsController extends Controller
 {
     public function get_paginated_news(Request $request)
     {
-        return News::paginated();
+        return News::paginate();
     }
 }
