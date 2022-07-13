24e4eef111753976f19ef21659f61cea8264a939
faisal <faisal@mvp-apps.ae>
2022-07-13 14:49:22 +0400

    Initial commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function get_paginated_news(Request $request)
+    {
+        return News::paginated();
+    }
+}
